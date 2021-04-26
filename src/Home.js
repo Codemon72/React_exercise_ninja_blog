41fccee09a0625f117f1258be210c8442265a855
Clemens Bruesch <clemensbruesch@gmail.com>
2021-04-26 10:07:41 +0200

    error handling in fetch

--- src/Home.js
+++ src/Home.js
@@ -5,23 +5,34 @@ const Home = () => {
 
   const [blogs, setBlogs] = useState(null);
   const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     fetch('http://localhost:8000/blogs')
       .then(res => {
+        // check if API endpoint ok
+        if (!res.ok) {
+          throw Error('could not fetch data from that endpoint');
+        }
         return res.json();
       })
       .then((data) => {
         setBlogs(data);
         setIsPending(false);
+        setError(null);
       })
+      .catch(err => {
+        setIsPending(false);
+        setError(err.message);
+      });
   }, []);
 
 
   return (
     <div className="home">
-      {isPending && <div>Loading...</div>}
-      {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
+      { error && <div>{ error }</div> }
+      { isPending && <div>Loading...</div> }
+      { blogs && <BlogList blogs={blogs} title="All Blogs!" /> }
     </div>
   )
 }
