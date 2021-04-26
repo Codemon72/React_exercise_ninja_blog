3996fd3ca53dbddae41bbb6dc6d38e4f5e2cda91
Clemens Bruesch <clemensbruesch@gmail.com>
2021-04-26 09:17:02 +0200

    loading message

--- src/Home.js
+++ src/Home.js
@@ -4,6 +4,7 @@ import BlogList from './BlogList';
 const Home = () => {
 
   const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
 
   useEffect(() => {
     fetch('http://localhost:8000/blogs')
@@ -12,16 +13,15 @@ const Home = () => {
       })
       .then((data) => {
         setBlogs(data);
+        setIsPending(false);
       })
   }, []);
 
 
   return (
     <div className="home">
-      {blogs && <BlogList 
-        blogs={blogs} 
-        title="All Blogs!"
-        />}
+      {isPending && <div>Loading...</div>}
+      {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
     </div>
   )
 }
